a3e9d408ff6e2a0b6f7d6b86ff97b72b28a35182
tyreyn-laptop <athosikish33@gmail.com>
2024-02-03 00:55:49 +0100

    [ADD] File downloader and database initializer

---
+++ zintegrujemy-zadanie/Entities/Product.cs
@@ -0,0 +1,66 @@
+namespace Zintegrujemy_Zadanie.Entities
+{
+    /// <summary>
+    /// Product entity class.
+    /// </summary>
+    public class Product
+    {
+        #region Fields and Constants
+
+        /// <summary>
+        /// Unique ID of the product.
+        /// </summary>
+        public int ID { get; set; }
+
+        /// <summary>
+        /// Product SKU, unique value created by warehouse.
+        /// </summary>
+        public string SKU { get; set; }
+
+        /// <summary>
+        /// Product name.
+        /// </summary>
+        public string name { get; set; }
+
+        /// <summary>
+        /// Product number.
+        /// </summary>
+        public string EAN { get; set; }
+
+        /// <summary>
+        /// Supplier name.
+        /// </summary>
+        public string producer_name { get; set; }
+
+        /// <summary>
+        /// Product Category.
+        /// </summary>
+        public string category { get; set; }
+
+        /// <summary>
+        /// Indicates whether the product is a wire 
+        /// (if value is 1).
+        /// </summary>
+        public int is_wire { get; set; }
+
+        /// <summary>
+        /// Indicates whether the product is available
+        /// for order (if value is 1).
+        /// </summary>
+        public int available { get; set; }
+
+        /// <summary>
+        ///  Indicates whether the product is shipped
+        ///  by supplier or warehouse. If value is 0,
+        ///  it’s shipped by warehouse, if 1,
+        ///  it’s shipped by supplier.
+        /// </summary>
+        public int is_vendor { get; set; }
+
+        /// <summary>
+        /// URL address to product’s image.
+        /// </summary>
+        public string default_image { get; set; }
+        #endregion
+    }
+}
